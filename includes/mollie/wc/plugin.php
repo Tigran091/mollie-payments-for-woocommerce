45da34f2507a98ea73f02b8ab996e2e2bab9b0de
l.vangunst <l.vangunst@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-10-02 09:02:26 +0000

    Tagging version 2.0.1
    git-svn-id: https://plugins.svn.wordpress.org/mollie-payments-for-woocommerce/tags/2.0.1@1258076 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- includes/mollie/wc/plugin.php
+++ includes/mollie/wc/plugin.php
@@ -6,7 +6,7 @@ class Mollie_WC_Plugin
 {
     const PLUGIN_ID      = 'mollie-payments-for-woocommerce';
     const PLUGIN_TITLE   = 'Mollie Payments for WooCommerce';
-    const PLUGIN_VERSION = '2.0.0-beta1';
+    const PLUGIN_VERSION = '2.0.1';
 
     /**
      * @var bool
@@ -21,6 +21,7 @@ class Mollie_WC_Plugin
         'Mollie_WC_Gateway_Belfius',
         'Mollie_WC_Gateway_Bitcoin',
         'Mollie_WC_Gateway_Creditcard',
+        'Mollie_WC_Gateway_DirectDebit',
         'Mollie_WC_Gateway_Ideal',
         'Mollie_WC_Gateway_MisterCash',
         'Mollie_WC_Gateway_PayPal',
